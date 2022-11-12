e81408c8decc2d5c5d2e0ddc0c21844c825c8fbf
Sammily <sammily03@yandex.ru>
2022-11-12 15:57:58 +0300

    feat: change connect string

--- Project management application/src/index.ts
+++ Project management application/src/index.ts
@@ -6,7 +6,7 @@ import * as serverService from './services/server.service';
 
 (async () => {
   try {
-    await mongoose.connect('mongodb+srv://${UserName}:${Password}@${clasterInfo}.mongodb.net/managerApp');
+    await mongoose.connect('mongodb+srv://dbadmin:qQ7k1uBS0dwi2USp@cluster0.s0y9tg7.mongodb.net/?retryWrites=true&w=majority');
     serverService.server.listen(process.env.PORT || PORT, function () {
       console.log('Сервер ожидает подключения...');
     })
